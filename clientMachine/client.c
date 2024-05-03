8fd553a593ea12e797aa677645fd50a6e2bd763e
root <root@DESKTOP-IBN72CO>
2024-05-03 03:19:10 +0100

    Code cleaned up

--- clientMachine/client.c
+++ clientMachine/client.c
@@ -24,31 +24,12 @@ const char* getFileName(const char* path) {
         return filename + 1;
 }
 
-int main(int argc , char *argv[])
-{
-    int SID;
+int connectToServer(int* SID) {
     struct sockaddr_in server;
-    char filePath[500];
-    char fileName[30];
-    char serverMessage[500];
-    char fileBuffer[FILE_BUFFER_SIZE] = {0};
-    int fd;
-    struct stat fileStat;
-    char fileSize[256];
-    int sentBytes = 0;
-    long int offset;
-    int remainData;
 
-    if (argc != 2) {
-        printf("Invalid number of command line arguments entered\n");
-        return 1;
-    }
-
-    strcpy(filePath, argv[1]);
-     
     //Create socket
-    SID = socket(AF_INET , SOCK_STREAM , 0);
-    if (SID == -1){
+    *SID = socket(AF_INET , SOCK_STREAM , 0);
+    if (*SID == -1){
         printf("Error creating socket.\n");
     } else {
     	printf("socket created.\n");
@@ -60,49 +41,85 @@ int main(int argc , char *argv[])
     server.sin_family = AF_INET; // IPV4 protocol
     
     //Connect to server
-    if (connect(SID , (struct sockaddr *)&server , sizeof(server)) < 0) {
+    if (connect(*SID , (struct sockaddr *)&server , sizeof(server)) < 0) {
         printf("connect failed. Error\n");
         return 1;
     }
-     
-    printf("Connected to server ok!!\n");
-        
+
+    printf("Connected to server ok.\n");
+    return 0;
+}
+
+int sendFile(int* SID, char* filePath) {
+    int fd;
+    struct stat fileStat;
+    char fileSize[256];
+    int sentBytes = 0;
+    long int offset;
+    int remainData;
+    
     //Reading file
     fd = open(filePath, O_RDONLY);
     if (fd == -1) {
         printf("File path invalid\n");
+        return 1;
     }
-
 
     //Get file stats
     if (fstat(fd, &fileStat) < 0) {
         printf("Error getting file stats\n");
+        return 1;
     } 
 
     //Send file name
-    send(SID, getFileName(filePath), FILE_BUFFER_SIZE, 0);
+    send(*SID, getFileName(filePath), FILE_BUFFER_SIZE, 0);
     
-    
+    //Send file size
     sprintf(fileSize, "%ld", fileStat.st_size); //Put file size in a buffer
-    send(SID, fileSize, sizeof(fileSize), 0); //send file size
-
+    send(*SID, fileSize, sizeof(fileSize), 0); //send file size
 
     offset = 0;
     remainData = fileStat.st_size;
 
-    /* Sending file data */
-    while (((sentBytes = sendfile(SID, fd, &offset, BUFSIZ)) > 0) && (remainData > 0))
+    //Sending file data 
+    while (((sentBytes = sendfile(*SID, fd, &offset, BUFSIZ)) > 0) && (remainData > 0))
     {
         fprintf(stdout, "1. Server sent %d bytes, Offset: %ld, Remaining data: %d\n", sentBytes, offset, remainData);
         remainData -= sentBytes;
         fprintf(stdout, "2. Server sent %d bytes, Offset: %ld, Remaining data: %d\n", sentBytes, offset, remainData);
     }
+    
+    return 0;
+}
 
 
-    if( recv(SID , serverMessage , 500 , 0) < 0)
-    {
-        printf("IO error.\n");
-        //break;
+int main(int argc , char *argv[])
+{
+    int SID;
+    char filePath[500];
+    char fileName[30];
+    char serverMessage[500];
+    char fileBuffer[FILE_BUFFER_SIZE] = {0};
+
+
+    if (argc != 2) {
+        printf("Invalid number of command line arguments entered\n");
+        return 1;
+    }
+
+    if (connectToServer(&SID) == 1) {
+        return 1;
+    }
+
+    strcpy(filePath, argv[1]);
+    if (sendFile(&SID, filePath) == 1) {
+        printf("Error sending file");
+        return 1;
+    }
+
+    if( recv(SID , serverMessage , 500 , 0) < 0) {
+        printf("Error receiving status message from server.\n");
+        return 1;
     }
 
     printf("%s\n", serverMessage);
--- serverMachine/server.c
+++ serverMachine/server.c
@@ -8,25 +8,12 @@
 #define PORT 8081
 #define FILE_BUFFER_SIZE 1024
 
-int main(int argc , char *argv[])
-{
-    int serverSocket; // socket descriptor
-    int clientSocket; // Client Socket
-    int connSize; // Size of struct 
-    int READSIZE;  // Size of sockaddr_in for client connection
-    char buffer[BUFSIZ];
-    char fileName[FILE_BUFFER_SIZE];
-    FILE* file;
-    int fileSize;
-    ssize_t len;
-    int remainData = 0;
+int bindServerSocket(int *serverSocket) {
+    struct sockaddr_in server;
 
-    struct sockaddr_in server , client;
-    char message[500];
-     
     //Create socket
-    serverSocket = socket(AF_INET , SOCK_STREAM , 0);
-    if (serverSocket == -1)
+    *serverSocket = socket(AF_INET , SOCK_STREAM , 0);
+    if (*serverSocket == -1)
     {
         printf("Could not create socket.\n");
     } else {
@@ -40,33 +27,45 @@ int main(int argc , char *argv[])
     // When INADDR_ANY is specified in the bind call, the  socket will  be bound to all local interfaces. 
      
     //Bind
-    if( bind(serverSocket,(struct sockaddr *)&server , sizeof(server)) < 0) {
+    if( bind(*serverSocket,(struct sockaddr *)&server , sizeof(server)) < 0) {
         perror("Bind issue.\n");
         return 1;
     } else {
     	printf("Bind Complete.\n");
     }
-     
-    //Listen for a conection
-    listen(serverSocket,3); 
+    return 0;
+}
+
+int acceptIncomingConnection(int* clientSocket, int* serverSocket) {
+    struct sockaddr_in client;
+    int connSize; // Size of struct 
+    
     //Accept and incoming connection
-    printf("Waiting for incoming connection from Client>>\n");
     connSize = sizeof(struct sockaddr_in);
-     
     //accept connection from an incoming client
-    clientSocket = accept(serverSocket, (struct sockaddr *)&client, (socklen_t*)&connSize);
-    if (clientSocket < 0) {
+    *clientSocket = accept(*serverSocket, (struct sockaddr *)&client, (socklen_t*)&connSize);
+    if (*clientSocket < 0) {
         perror("Can't establish connection");
         return 1;
     } else {
     	printf("Connection from client accepted.\n");
     }
+    return 0;
+}
+
+int receiveFile(int* clientSocket) {
+    char fileName[FILE_BUFFER_SIZE];
+    char buffer[BUFSIZ];
+    FILE* file;
+    int fileSize;
+    ssize_t len;
+    int remainData = 0;
 
     //Receive file name
-    recv(clientSocket, fileName, FILE_BUFFER_SIZE, 0);
+    recv(*clientSocket, fileName, FILE_BUFFER_SIZE, 0);
 
     //Recevie file size
-    recv(clientSocket, buffer, BUFSIZ, 0);
+    recv(*clientSocket, buffer, BUFSIZ, 0);
     fileSize = atoi(buffer);
 
     //Open file for writing
@@ -76,15 +75,42 @@ int main(int argc , char *argv[])
         return 1;
     }  
 
+    //Receive file
     remainData = fileSize;
-
-    while ((remainData > 0) && ((len = recv(clientSocket, buffer, BUFSIZ, 0)) > 0))
+    while ((remainData > 0) && ((len = recv(*clientSocket, buffer, BUFSIZ, 0)) > 0))
     {
         fwrite(buffer, sizeof(char), len, file);
         remainData -= len;
         fprintf(stdout, "Received %ld bytes, Remaning: %d bytes\n", len, remainData);
     }
-   
+
+}
+
+
+int main(int argc , char *argv[])
+{
+    int serverSocket; // socket descriptor
+    int clientSocket; // Client Socket
+    int READSIZE;  // Size of sockaddr_in for client connection
+    char message[500];
+        
+    if (bindServerSocket(&serverSocket) == 1) {
+        return 1;
+    }    
+     
+    //Listen for a conection
+    listen(serverSocket,3); 
+    printf("Waiting for incoming connection from Client...\n");
+
+    if (acceptIncomingConnection(&clientSocket, &serverSocket) == 1) {
+        return 1;
+    }
+
+    if (receiveFile(&clientSocket) == 1) {
+        printf("Error receiving file");
+        return 1;
+    }
+    
     send(clientSocket, "File Transferred Successfully\n", strlen("File Transferred Successfully\n"), 0);
 
     if(READSIZE == 0) {
